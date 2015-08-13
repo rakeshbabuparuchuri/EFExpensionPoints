6f67cdf814711314cbfb9f0548c20950b179c035
rakeshbabuparuchuri <rakeshbabu.paruchuri@outlook.com>
2015-08-13 15:52:04 +0530

    Summary
    Description

---
+++ ContosoUniversity/DAL/SchoolConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using ContosoUniversity.Helpers;
+using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolConfiguration : DbConfiguration
+    {
+        public SchoolConfiguration()
+        {
+            AddInterceptor(new SoftDeleteInterceptor());
+        }
+    }
+}
